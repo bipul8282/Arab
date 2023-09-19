0e9a5f14ec8efbbe96c9224f15f47715238f9103
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2023-09-19 17:22:29 +0530

    card icons adding

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { cardData } from '../Data/Data'
 import  './Cards.css'
+import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
+import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
 const Cards = () => {
   return (
     <div className='Limit'>
       {
         cardData.map((item, index) => (
-            <div key={index}>
+            <div  key={index}>
               <h3>{item.Name}</h3>
                <p>{item.previousPrice}</p>
                <p>{item.Price}</p>
                <button>{item.button}</button>
                <hr/>
                <span>{item.desc}</span>
+               <p><PersonOutlineRoundedIcon/>{item.icon}</p>
+               <p><CloudUploadOutlinedIcon/></p>
+               <p><MailOutlineOutlinedIcon/></p>
             </div>
         ))
       }
--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -2,39 +2,41 @@ export const cardData = [
     {
      Name : "Basic",
      previousPrice: "$56.99",
-     Price: "$9.99/mon",
+     Price: "$9.99/mo",
      button: "Get Started",
-     desc: "What you will get",
+     desc: "What you will get :",
+     icon:"Upto 25Users",
 
     },
     {
-        Name : "Basic",
+        Name : "Standard",
         previousPrice: "$56.99",
-        Price: "$9.99/mon",
+        Price: "$99.99/mo",
         button: "Get Started",
-        desc: "What you will get",
+        desc: "What you will get :",
+        icon:"Upto 50 Users",
    
        },
        {
-           Name : "Basic",
+           Name : "Premium",
            previousPrice: "$56.99",
-           Price: "$9.99/mon",
+           Price: "$199.99/mo",
            button: "Get Started",
-           desc: "What you will get",
+           desc: "What you will get :",
+           icon:"Upto 75 Users",
       
           },
           {
-              Name : "Basic",
-              previousPrice: "$56.99",
-              Price: "$9.99/mon",
+              Name : "Free Starter",
               button: "Get Started",
-              desc: "What you will get",
+              desc: "What you will get :",
+              icon:"Upto 8 Users",
          
              },
-             { Name : "Basic",
-             previousPrice: "$56.99",
-             Price: "$9.99/mon",
+             { Name : "Enterprise Plan",
              button: "Get Started",
-             desc: "What you will get",
+             desc: "What you will get :",
+             icon:"More than 75 Users",
+             
             }
 ]

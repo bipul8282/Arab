2c8bd238e87ba9022fd632c971d46df84d903e1e
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2023-09-20 09:17:21 +0530

    almost completed

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Starter } from '../Data/Data';
 import './Cards.css';
-import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
-import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
-import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 
 const Card = () => {
@@ -15,26 +12,13 @@ const Card = () => {
             <h3>{item.Name}</h3>
             <p style={{ textDecoration: 'line-through' }}>{item.previousPrice}</p>
             <p>{item.Price}</p>
-            {/* <p>{item.subtitle}</p> */}
             <button style={{ backgroundColor: 'lightgreen' }}>{item.button}</button>
           </div>
           <div>
             <span>{item.desc}</span>
             <p>
-              <PersonOutlineRoundedIcon />
-              {item.icon}
-            </p>
-            <p>
-              <CloudUploadOutlinedIcon />
-              {item.Storage}
-            </p>
-            <p>
-              <MailOutlineOutlinedIcon />
-              {item.email}
-            </p>
-            <p>
-                <DoneAllIcon/>
-                {item.tick}
+              <DoneAllIcon />
+              {item.tick}
             </p>
           </div>
         </div>
